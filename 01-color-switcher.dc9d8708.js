400d42c9b5fdcf422ffaaed5c0aca4deafc99bf4
olekspyl <99043767+olekspyl@users.noreply.github.com>
2022-08-18 22:16:23 +0000

    Deploying to gh-pages from @ olekspyl/goit-js-hw-09@9304306398108ca5cdd97cc0969370841895dc7d 🚀

---
+++ 01-color-switcher.dc9d8708.js
@@ -0,0 +1,2 @@
+!function(){var t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]"),body:document.querySelector("body")};t.startBtn.addEventListener("click",(function(){n=setInterval((function(){t.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.startBtn.disabled=!0})),t.stopBtn.addEventListener("click",(function(){clearInterval(n),t.startBtn.disabled=!1}));var n=null}();
+//# sourceMappingURL=01-color-switcher.dc9d8708.js.map
---
+++ 01-color-switcher.fdb98237.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]"),body:document.querySelector("body")};t.startBtn.addEventListener("click",(function(){e=setInterval((()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.startBtn.disabled=!0})),t.stopBtn.addEventListener("click",(function(){clearInterval(e),t.startBtn.disabled=!1}));let e=null;
+//# sourceMappingURL=01-color-switcher.fdb98237.js.map
