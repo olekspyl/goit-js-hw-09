e30917a9074d98d1cb04eb51ac7751940439d583
Oleksandra Pylypchuk <lemocream@gmail.com>
2022-08-21 15:37:50 +0200

    update

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -3,46 +3,40 @@ const refs = {
   delayEl: document.querySelector('input[name="delay"]'),
   stepEl: document.querySelector('input[name="step"]'),
   amountEl: document.querySelector('input[name="amount"]'),
-  btnEl: document.querySelector('button'), 
+  btnEl: document.querySelector('button'),
 }
-
 refs.formEl.addEventListener('submit', onFormSubmit);
-let summaryDelay;
-let position; 
 
 
 function onFormSubmit(event) {
   event.preventDefault();
 
   const amount = Number(refs.amountEl.value);
-  const delay = Number(refs.delayEl.value);
+  let delay = Number(refs.delayEl.value);
   const step = Number(refs.stepEl.value);
-  summaryDelay = delay + step;
-  
-  for (let i = 0; i <= amount; i += 1) {
-       createPromise(position, summaryDelay)
-      .then(({ position, summaryDelay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${summaryDelay}ms`);
+  let position = null;
+
+
+  for (  position = 1; position <= amount; position += 1) {
+       createPromise(position, delay)
+      .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
   })
-      .catch(({ position, summaryDelay }) => {
-    console.log(`❌ Rejected promise ${position} in ${summaryDelay}ms`);
+      .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
   });
+ delay += step;
    }
 }
-
-function createPromise() {
-    return new Promise((resolve, reject) => {
-
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
       const shouldResolve = Math.random() > 0.3;
-        if (shouldResolve) {
-          resolve({ position, summaryDelay });
-  } else {
-          reject({ position, summaryDelay });
-  }
-    }, summaryDelay)
-  });
- 
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay)
+  })
 }
-
-
